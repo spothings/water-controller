4ca187adc0bd0491b618576ded89dd69ddb07955
bukanspot <spot@tracert.id>
2021-12-20 22:48:58 +0800

    distance

--- water-controller/water-controller.ino
+++ water-controller/water-controller.ino
@@ -7,6 +7,15 @@
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
+const int trigPin = 5;
+const int echoPin = 18;
+
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+
+long duration;
+float distanceCm;
+
 void setup() {
   Serial.begin(115200);
   Serial.print("Connecting Wifi: ");
@@ -27,8 +36,30 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   bot.sendMessage(CHAT_ID, "Bot started up", "");
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
   
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  
+  delay(1000);
 }
